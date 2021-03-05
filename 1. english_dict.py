0a7e46ab5e55e8518e189c25f5d654a1d05cb21c
shyamdk <shyamdk@gmail.com>
2021-03-05 12:38:11 +0530

    0503

---
+++ 1. english_dict.py
@@ -0,0 +1,26 @@
+import json
+from difflib import get_close_matches
+
+data = json.load(open("files/1data.json"))
+
+def get_meaning(w):
+    if w in data:
+        return data[w] 
+    elif len(get_close_matches(w, data.keys())) > 0:
+        yn = input(" Did you mean %s instead? Enter Y if yes, or N if no:" % get_close_matches(w, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(w, data.keys())[0]]
+        elif yn == "N":
+            return "The word does not exist, please check again."
+        else:
+            return "We did not understand your input"
+    else:
+        return "The word does not exist, please check again."
+word = input("Enter a word: ")
+
+output = get_meaning(word.lower())
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
